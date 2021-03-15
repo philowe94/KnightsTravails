944e3023739396d5363e0198cdee5f6c342a2075
Philip Lowe <philip.lowe94@gmail.com>
2021-03-15 13:57:29 -0400

    phase1: new_move_positions

--- lib/KnightPathFinder.rb
+++ lib/KnightPathFinder.rb
@@ -1,2 +1,53 @@
+require_relative "./00_tree_node.rb"
+
 class KnightPathFinder
+   
+    attr_reader :considered_positions
+    #pos: [x,y] #starting position
+    def initialize(start_pos)
+        @start_pos = start_pos
+        #move tree
+        #values in this tree are positions
+        #the children of a node are positions a knight can move to from
+        #the parent
+        @root_node = PolyTreeNode.new(start_pos)   
+        
+        #considered_positions are all previously considered positions
+        @considered_positions = [start_pos]
+        
+        #this method will build out the move tree from the root node
+        #self.build_move_tree
+    end
+
+    #given a pos,
+    #return an array of the up to 8 positions the knight can move to
+    def valid_moves(pos)
+        x,y = pos
+        returnarray = []
+        moves = [   [2,  1],[1,  2],
+                    [2, -1],[1, -2],
+                    [-2, 1],[-1, 2],
+                    [-2,-1],[-1,-2]]
+
+        moves.each do |move|
+            xchange, ychange = move
+            returnarray << [x+xchange, y+ychange]
+        end
+
+        return returnarray.select do |pos|
+            x,y = pos
+            x > 0 && x < 8 && y > 0 && y < 8
+        end
+    end
+
+    #this is valid moves with all the previously considered positions filtered out
+    def new_move_positions(pos)
+        new_positions = valid_moves(pos).select do |pos| 
+            !@considered_positions.include?(pos)
+        end
+        @considered_positions += new_positions
+        return new_positions
+    end
+
+
 end
